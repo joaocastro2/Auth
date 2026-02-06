eff550db2f7d9c7a08b99507768f061890729da0
joaocastro2 <joaocastrocntt06@gmail.com>
2026-02-06 12:06:51 -0300

    REFACTOR: Flyway migrations ok

---
+++ src/main/java/com/example/auth/config/FlywayMigrationConfig.java
@@ -0,0 +1,29 @@
+package com.example.auth.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayMigrationConfig {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUser;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(datasourceUrl, datasourceUser, datasourcePassword)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        return flyway;
+    }
+}
